bb7359820862e35cf746d937601283a48803f1dc
Vitor <catchtherainbow33@gmail.com>
2022-04-05 12:57:32 -0300

    dropdownmenu refatorado

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
-
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"');
 scrollSuave.init();
@@ -32,8 +31,9 @@ fetchBitcoin('https://blockchain.info/ticker','.btc-preco');
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]')
 scrollAnima.init()
 
+const dropdownMenu = new DropdownMenu('[data-dropdown]')
+dropdownMenu.init()
 
-initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 
